321a27b670473f8baa94cbacda9d780802eca0ce
Paulo Eduardo <pauloeduardosozza73@gmailcom>
2024-03-04 11:11:22 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  
+
+  return (
+    <>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod eligendi iusto exercitationem veritatis impedit doloremque quasi, doloribus excepturi veniam voluptatibus at. Hic quasi facere ut, laudantium dolore eligendi ipsum cupiditate.</p>
+    </>
+  )
+}
+
+export default App
